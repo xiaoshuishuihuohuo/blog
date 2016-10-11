b84b06ed18225373aaad94f4bc79091beb83c79e
mzy <137473005@qq.com>
2016-10-11 22:00:28 +0800

    format code

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, session, g, redirect, url_for, render_template
 from . import main
-@main.route('/login')
-def login():
-	return render_template('index.html')
+
 
 @main.route('/')
 def index():
-	return redirect('/login')
+    return render_template('index.html')
+
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
