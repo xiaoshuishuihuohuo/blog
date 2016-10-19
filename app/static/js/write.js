ef820c0781bca07db367ac2f7d2a68e5731dbddf
温德盛 <wendesheng321@163.com>
2016-10-19 22:08:24 +0800

    定时保存（测试）

--- app/static/js/write.js
+++ app/static/js/write.js
@@ -9,9 +9,36 @@ $(document).ready(function() {
 		textarea: $('#txt-content'),
 		placeholder: 'write something',
 		toolbar: toolbar,
+		defaultImage: '../lib/simditor-2.3.6/images/image.png',
 		pasteImage: true,
-		upload: true
+		upload : {
+            url : '', //文件上传的接口地址
+            params: null, //键值对,指定文件上传接口的额外参数,上传的时候随文件一起提交
+            fileKey: 'fileDataFileName', //服务器端获取文件数据的参数名
+            connectionCount: 3,
+            leaveConfirm: '正在上传文件'
+        }
 	});
 
-
+	
 });
+
+var time = new Date();
+var count = 0, saveTime = 5; 
+
+saveDraft();
+
+function saveDraft() {
+	count++;
+	if (count < saveTime) {
+		console.log(count);
+		$("#submit").val("发布");
+		setTimeout("saveDraft()", 1000);
+	} else {
+		count = 0;
+		// 保存操作
+
+		$("#submit").val("保存草稿中...");
+		setTimeout("saveDraft()", 1000);
+	}
+}
