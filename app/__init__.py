b79be2f49fbc59fa2f09ea8b73bede08ee82fcf3
mzy <137473005@qq.com>
2016-10-20 23:19:42 +0800

    login

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,29 @@
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+csrf = CsrfProtect()
+login_manager = LoginManager()
+
+
 def create_app():
-	app = Flask(__name__)
-	from .main import main as main_blueprint
-	app.register_blueprint(main_blueprint)
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint, url_prefix = '/auth')
-	
-	csrf = CsrfProtect()
-	csrf.init_app(app)
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY']='fuck'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:123@192.168.1.5/blog'
+    
+    db.init_app(app)
+    csrf.init_app(app)
+    login_manager.init_app(app)
 
-	login_manager = LoginManager()
-	login_manager.init_app(app)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-	app.config['SECRET_KEY']='fuck'
-	return app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+    
+    return app
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,28 +1,46 @@
 from flask import (Flask, request, session, g, redirect, url_for, render_template,Blueprint,flash)
 from . import auth
-from .forms import signinForm
+from .forms import SigninForm, RegForm
 import json
+from ..models import User
+from .. import db
+from flask_login import login_user
+
+
 @auth.route('/signin', methods=['POST'])
 def signin():
-	# username = request.form.get('username')
-	form = signinForm()
-	if form.validate_on_submit():
-		if form.username.data == 'fuck':
-			flash('don\'t use fuck' )
-			return redirect(url_for('main.login'))
-		return redirect(url_for('main.mainpage'))
-	first_error = form.errors.values()[0][0]
-	flash(first_error)
-	return redirect(url_for('main.login'))
-	
+    # username = request.form.get('username')
+    form = SigninForm()
+    a= form.remember_me.data
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user != None:
+            login_user(user,form.remember_me.data)
+            return redirect(url_for('main.main_page'))
+        else:
+            flash('error')
+            return redirect(url_for('main.login'))
+    first_error = form.errors.values()[0][0]
+    flash(first_error)
+    return redirect(url_for('main.login'))
 
-# @auth.route('/regist', methods=['POST'])
-# def regist():
-#     form = regForm()
-# 	if form.validate_on_submit():
-# 		return redirect('/mainpage')
-# 	message = form.errors.values()[0][0]
-# 	print message
-# 	flash(message)
-# 	return redirect(url_for('main.login'))
+
+@auth.route('/regist', methods=['POST'])
+def regist():
+    form = RegForm()
+    a = form.signup_password.data
+    if form.validate_on_submit():
+        user = User(passwd=form.signup_password.data)
+        user.username = form.signup_username.data
+        user.email_addr = '123123@qq.com'
+        user.login_name = 'mzy'
+
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        return redirect(url_for('main.main_page'))
+    message = form.errors.values()[0][0]
+    print message
+    flash(message)
+    return redirect(url_for('main.login'))
 
