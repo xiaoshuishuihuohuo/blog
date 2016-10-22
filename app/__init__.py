5737e7bb78297c33eaf32fe5eb2af466cce2a149
mzy <137473005@qq.com>
2016-10-22 09:53:46 +0800

    config

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_wtf.csrf import CsrfProtect
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import os
+from config import Config
 
 db = SQLAlchemy()
 csrf = CsrfProtect()
@@ -11,11 +12,11 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY']='fuck'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:123@192.168.1.5/blog'
-    app.config['IMG_SAVE_PATH'] = 'f:/asd/'
+    app.config.from_object(Config)
+    # app.config['SECRET_KEY']='fuck'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:123@192.168.1.5/blog'
+    # app.config['IMG_SAVE_PATH'] = 'f:/asd/'
 
     if not os.path.exists(app.config['IMG_SAVE_PATH']):
         os.makedirs(app.config['IMG_SAVE_PATH'])
