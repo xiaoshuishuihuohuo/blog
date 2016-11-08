b991bc8c35b708a0f7317ea487e0992fcca5b856
sg <wendesheng321@163.com>
2016-11-08 14:29:20 +0800

    评论，回复评论（初步）

--- app/static/js/article.js
+++ app/static/js/article.js
@@ -10,4 +10,45 @@ $(document).ready(function () {
             $("#toggle-comment img").attr("src", "../static/img/arrow-down.png");
         }
     });
+
+    $(".comment-body").hover(function () {
+        $(".comment-reply-btn").show();
+    },function () {
+        $(".comment-reply-btn").hide();
+    });
+
+    $(".comment-body").click(function () {
+        $(".reply-comment").toggle();
+        /*$(".reply-comment").toggleClass("active");
+        var isActive = $(".reply-comment").attr("class").indexOf("active");
+        if (isActive > 0) {
+            $(".reply-comment").hide();
+        } else {
+            $(".reply-comment").show();
+        }*/
+    });
+
+    $("#make-comment button").click(function () {
+        if ($("#make-comment textarea").val() == "") {
+            $("#make-comment textarea").focus();
+            return false;
+        }
+
+        $.post(
+            url,
+            {
+                comment: $("#make-comment textarea").val()
+            },
+            function (data) {
+                if (success) {
+                    console.log('在评论区添加这条评论');
+                }
+            },
+            'json'
+        );
+    });
 });
+
+function reply(id) {
+    
+}
