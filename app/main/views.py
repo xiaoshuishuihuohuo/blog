b8da5b596b89c5a5ea8f45d09ea868a2017482f4
mzy <137473005@qq.com>
2017-02-13 22:59:43 +0800

    首页top10排序 删除文章修复 发布文章redis时间格式化

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,7 @@ def index():
     #热门文章redis实现
     result = []
     try:
-        top = redis.zrange('pageviews', 0, 10)
+        top = redis.zrange('pageviews', 0, 10, desc=True)
         if top:
             info_list = redis.hmget('articles', top)
             for article in info_list:
@@ -23,7 +23,7 @@ def index():
                     tmp.id = info_dict['id']
                     tmp.title = info_dict['title']
                     tmp.last_modified_time = info_dict['last_modified_time']
-
+                    tmp.author = info_dict['author']
                     tmp.pageviews = int(redis.zscore('pageviews', tmp.id))
                     #TODO like count
                     tmp.like_count = 0
